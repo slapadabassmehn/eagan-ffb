7042d9608e97f0eaa3b56604c5f00c794472135b
slapadabassmehn <74695287+slapadabassmehn@users.noreply.github.com>
2024-07-30 00:01:57 -0500

    ddd

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -98,7 +98,9 @@
 <a href="https://drive.google.com/file/d/1lylnsoLnL9iqk5HD3snH5V467_OwjpBh/view?usp=sharing">Contitution download</a>
 
 <div style={{display: "flex", justifyContent: "center"}}>
-    <iframe 
+    <iframe
+        width="100%" 
+        height="100%"
         src="https://docs.google.com/document/d/e/2PACX-1vRwxAlRjpgx5zTi4LsTQnNCMjwuJQg_AvEE45Y1d-iUIazg3tzAStaxWwrpu29gtmCuG5jY586GR8jp/pub?embedded=true"
     ></iframe>
 </div>    
