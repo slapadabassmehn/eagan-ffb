60a34767812858b0d9bd88708f6fe79dfa1ee511
slapadabassmehn <74695287+slapadabassmehn@users.noreply.github.com>
2024-08-12 23:59:36 -0500

    fixed smithers

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -147,7 +147,7 @@ export const managers = [
       // "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Rosemount", // (optional)
       "bio": "Lorem ipsum...",
-      "photo": "/managers/smithers.png", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/smithers.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2009, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "gb", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
