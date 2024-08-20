c6df455ab26025743aa469fec7662a4e2fb89856
slapadabassmehn <74695287+slapadabassmehn@users.noreply.github.com>
2024-08-20 00:12:11 -0500

    managers line 28 and 31

--- src/routes/managers/+page.svelte
+++ src/routes/managers/+page.svelte
@@ -25,8 +25,10 @@
         max-width: 500px;
         margin: 80px auto;
     }
+    document.getElementById('bio').innerHTML = manager.bio;
 </style>
 
+<div id="bio"></div>
 <div class="main">
     {#await leagueTeamManagersData}
         <!-- promise is pending -->
