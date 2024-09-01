dee67dfae194c3fd0fceb2126713bfac824007f4
slapadabassmehn <74695287+slapadabassmehn@users.noreply.github.com>
2024-09-01 00:09:19 -0500

    updated jpeg

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -286,7 +286,7 @@ export const managers = [
         "rival": {
           name: "Everyone", // Can be anything (usually your rival's name)
           link: 6, // manager array number within this array, or null to link back to all managers page
-          image: "/managers/_everyone.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+          image: "/managers/_everyone.jpeg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
         },
         "favoritePlayer": 6797, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
         "valuePosition": "idk", // (optional) Favorite position (QB, WR, RB, TE, etc.)
