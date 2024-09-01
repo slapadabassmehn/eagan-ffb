6b45f5cc6e3ca102c1fc9ce55d093ac2b1f87a9c
slapadabassmehn <74695287+slapadabassmehn@users.noreply.github.com>
2024-09-01 01:37:57 -0500

    cheap #2

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -267,7 +267,7 @@ export const managers = [
           image: "/managers/tim.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
         },
         "favoritePlayer": 6943, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
-        "valuePosition": "Picks", // (optional) Favorite position (QB, WR, RB, TE, etc.)
+        "valuePosition": "Cheap", // (optional) Favorite position (QB, WR, RB, TE, etc.)
         "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
         "philosophy": "Get performance anxiety, blame it on his team. Slowly rebuild a behemoth.",
         "tradingScale": 10, // 1 - 10
