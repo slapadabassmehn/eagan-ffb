992d31b25d383ba94d9bf19aba16427fac70b892
slapadabassmehn <74695287+slapadabassmehn@users.noreply.github.com>
2024-11-20 09:29:14 -0600

    week 11

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -76,18 +76,18 @@ export const homepageText = `
                 </tr>
             </thead>
             <tbody>
-                <tr><td>Tyler</td><td>32</td><td>1365.13</td><td>8</td><td>2</td></tr>
-                <tr><td>John</td><td>29</td><td>1221.29</td><td>7</td><td>3</td></tr>
-                <tr><td>Tim</td><td>27</td><td>1212.4</td><td>7</td><td>3</td></tr>
-                <tr><td>Vollmuth</td><td>25</td><td>1159.12</td><td>6</td><td>4</td></tr>
-                <tr><td>EVB</td><td>24</td><td>1212.04</td><td>6</td><td>4</td></tr>
-                <tr><td>Josh</td><td>22</td><td>1158.84</td><td>5</td><td>5</td></tr>
-                <tr><td>Gorman</td><td>19</td><td>1092.7</td><td>5</td><td>5</td></tr>
-                <tr><td>Charlie</td><td>18</td><td>1124.59</td><td>5</td><td>5</td></tr>
-                <tr><td>Lindberg</td><td>16</td><td>1061.27</td><td>4</td><td>6</td></tr>
-                <tr><td>Henry</td><td>11</td><td>947.38</td><td>3</td><td>7</td></tr>
-                <tr><td>Anders</td><td>9</td><td>952.04</td><td>2</td><td>8</td></tr>
-                <tr><td>Smithers</td><td>8</td><td>940.85</td><td>2</td><td>8</td></tr>
+                <tr><td>Tyler</td><td>36</td><td>1511.51</td><td>9</td><td>2</td></tr>
+                <tr><td>John</td><td>33</td><td>1359.87</td><td>8</td><td>3</td></tr>
+                <tr><td>Vollmuth</td><td>29</td><td>1325.46</td><td>7</td><td>4</td></tr>
+                <tr><td>Tim</td><td>27</td><td>1307.78</td><td>7</td><td>4</td></tr>
+                <tr><td>EVB</td><td>24</td><td>1285.46</td><td>6</td><td>5</td></tr>
+                <tr><td>Josh</td><td>23</td><td>1279.48</td><td>5</td><td>6</td></tr>
+                <tr><td>Charlie</td><td>22</td><td>1259.46</td><td>6</td><td>5</td></tr>
+                <tr><td>Gorman</td><td>19</td><td>1161.22</td><td>5</td><td>6</td></tr>
+                <tr><td>Lindberg</td><td>17</td><td>1192.27</td><td>4</td><td>7</td></tr>
+                <tr><td>Henry</td><td>14</td><td>1079.61</td><td>4</td><td>7</td></tr>
+                <tr><td>Anders</td><td>12</td><td>1080.67</td><td>3</td><td>8</td></tr>
+                <tr><td>Smithers</td><td>8</td><td>995.03</td><td>2</td><td>9</td></tr>
               </tbody>
         </table>
     </div>
