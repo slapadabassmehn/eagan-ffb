aecbc6feea8d3b2fbb47417d445f5d27a3aafc56
slapadabassmehn <74695287+slapadabassmehn@users.noreply.github.com>
2025-07-04 16:02:20 -0500

    updated to 2025

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1055729872682852352"; // your league ID
+export const leagueID = "1208621417810116608"; // your league ID
 export const leagueName = "Eagan FFB"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
